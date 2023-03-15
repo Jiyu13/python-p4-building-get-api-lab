c23a3f10447dd659c725b267df96f28151627879
Ziru Zou <jiyucoding@yahoo.com>
2023-03-14 21:25:18 -0400

    add columns, relationships and serialize_rules for tables

--- server/models.py
+++ server/models.py
@@ -9,7 +9,32 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 class Bakery(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "bakeries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    # 1-m relationship between bakery and baked_goods => baked_goods.bakery
+    baked_goods = db.relationship("BakedGood", backref="bakery")
+    serialize_rules = ('-baked_goods.bakery',)
+
+    def __repr__(self):
+        return f"""<Bakery {self.id}: {self.name}"""
+
 
 class BakedGood(db.Model, SerializerMixin):
-    pass
+    __tablename__ = "baked_goods"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"))
+    serialize_rules = ('-bakery.baked_goods',)
+
+    def __repr__(self):
+        return f"""<BakedGood {self.id}: {self.name}, price: {self.price}"""
